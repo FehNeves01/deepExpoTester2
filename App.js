3f0a569072f93dded37b9f6bce72ce256c0e131b
Felipe Neves <felipeneves@Mini-de-Felipe.cpv1.com.br>
2021-07-20 16:23:40 -0300

    COnfigurado 2 btn e testados

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//Importando dependencias Expo
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <button title="Open With Link" onPress={() => Linking.openURL('https://github.com/FehNeves01')} />
+      <button title="Open Web Browser" onPress={() => WebBrowser.openBrowserAsync('https://github.com/FehNeves01')} />
     </View>
   );
 }
