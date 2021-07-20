51c391b5e38e613999df9192730856e658d78bd4
Felipe Neves <felipeneves@Mini-de-Felipe.cpv1.com.br>
2021-07-20 17:06:36 -0300

    Funcionando URL via EXP://

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 //Importando dependencias Expo
 import * as Linking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
 
 export default function App() {
+
+  const [data, setData] = useState('');
+
+  function handleDeepLink(event){
+  let data = Linking.parse(event.url);
+  setData(data);
+}
+
+  useEffect(()=>{
+    async function getInicialURL(){
+      const inicialURL = await Linking.getInitialURL();
+      if(inicialURL) setData(Linking.parse(inicialURL))
+    }
+
+    Linking.addEventListener("url", handleDeepLink);
+    if(!data){
+      getInicialURL();
+    }
+    
+    return(()=>{
+      Linking.removeEventListener("url");
+    })
+  },[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
       <StatusBar style="auto" />
-      <button title="Open With Link" onPress={() => Linking.openURL('https://github.com/FehNeves01')} />
-      <button title="Open Web Browser" onPress={() => WebBrowser.openBrowserAsync('https://github.com/FehNeves01')} />
+      <View style={styles.viewBtn}>
+        <Text>{data? JSON.stringify(data): "app not opened from deep link "}</Text>
+      </View>
+      <View style={styles.viewBtn}>
+        <Button  title="Open With Link" onPress={() => Linking.openURL("https://github.com/FehNeves01")} />
+        <Button title="Open Web Browser" onPress={() => WebBrowser.openBrowserAsync('https://github.com/FehNeves01')} />
+      </View>
     </View>
   );
 }
@@ -23,4 +52,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  viewBtn: {
+    marginTop: 15,
+  }
 });
